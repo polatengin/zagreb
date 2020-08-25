d0b658949431b643744c50def7ae9ffd22ca1df5
polatengin <polatengin@outlook.com>
2020-08-25 20:30:09 +0000

    label structure defined

--- src/WebHookPayload.cs
+++ src/WebHookPayload.cs
@@ -90,7 +90,7 @@ namespace zagreb
     public object[] RequestedTeams { get; set; }
 
     [JsonProperty("labels")]
-    public object[] Labels { get; set; }
+    public Label[] Labels { get; set; }
 
     [JsonProperty("milestone")]
     public object Milestone { get; set; }
@@ -163,6 +163,30 @@ namespace zagreb
 
     [JsonProperty("changed_files")]
     public long ChangedFiles { get; set; }
+  }
+
+  public partial class Label
+  {
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("node_id")]
+    public string NodeId { get; set; }
+
+    [JsonProperty("url")]
+    public Uri Url { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("color")]
+    public string Color { get; set; }
+
+    [JsonProperty("default")]
+    public bool Default { get; set; }
+
+    [JsonProperty("description")]
+    public string Description { get; set; }
   }
 
   public partial class Base
