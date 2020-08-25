69e1100e83cdb2f41e28d6da5bf6852cb98ded66
polatengin <polatengin@outlook.com>
2020-08-25 20:03:05 +0000

    links structure defined

--- src/WebHookPayload.cs
+++ src/WebHookPayload.cs
@@ -3,6 +3,33 @@ using Newtonsoft.Json;
 
 namespace zagreb
 {
+  public partial class Links
+  {
+    [JsonProperty("self")]
+    public Comments Self { get; set; }
+
+    [JsonProperty("html")]
+    public Comments Html { get; set; }
+
+    [JsonProperty("issue")]
+    public Comments Issue { get; set; }
+
+    [JsonProperty("comments")]
+    public Comments Comments { get; set; }
+
+    [JsonProperty("review_comments")]
+    public Comments ReviewComments { get; set; }
+
+    [JsonProperty("review_comment")]
+    public Comments ReviewComment { get; set; }
+
+    [JsonProperty("commits")]
+    public Comments Commits { get; set; }
+
+    [JsonProperty("statuses")]
+    public Comments Statuses { get; set; }
+  }
+
   public partial class Comments
   {
     [JsonProperty("href")]
