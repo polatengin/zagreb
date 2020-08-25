40fa88defe3facda6b503bc39c3dd973058383af
polatengin <polatengin@outlook.com>
2020-08-25 20:08:05 +0000

    initial source file

---
+++ src/PRListenerFunction.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System;
+
+namespace zagreb
+{
+}
