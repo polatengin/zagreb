c89b41a80d980e6b84f5ac548110d876d6281437
polatengin <polatengin@outlook.com>
2020-08-26 16:01:33 +0000

    app service plan structure added

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,6 +7,7 @@ locals {
   project_name              = replace(lower(var.project_name == "" ? random_string.lower.result : var.project_name), " ", "")
   resource_group_name       = "${local.project_name}-rg"
   storage_account_name      = "${local.project_name}st"
+  app_service_plan_name     = "${local.project_name}-plan"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -28,4 +29,20 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE APP SERVICE PLAN
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = local.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
 }
