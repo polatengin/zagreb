cd7d49d4388a5b50e058762a842e7dbe53345546
polatengin <polatengin@outlook.com>
2020-08-26 16:01:02 +0000

    storage account structure added

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -6,6 +6,7 @@ provider "azurerm" {
 locals {
   project_name              = replace(lower(var.project_name == "" ? random_string.lower.result : var.project_name), " ", "")
   resource_group_name       = "${local.project_name}-rg"
+  storage_account_name      = "${local.project_name}st"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -17,3 +18,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE AZURE STORAGE ACCOUNT
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_storage_account" "storage" {
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
