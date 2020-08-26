aa1c88ce734779faac3ea100bff72bfb3e3a7c11
polatengin <polatengin@outlook.com>
2020-08-26 16:00:29 +0000

    resource group structure added

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -4,5 +4,16 @@ provider "azurerm" {
 }
 
 locals {
+  project_name              = replace(lower(var.project_name == "" ? random_string.lower.result : var.project_name), " ", "")
+  resource_group_name       = "${local.project_name}-rg"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE RESOURCE GROUP
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_resource_group" "rg" {
+  name     = local.resource_group_name
+  location = var.location
 }
 
