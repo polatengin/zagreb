1490a6fe4e87a693bb5a008e771e0c7bb40d3470
polatengin <polatengin@outlook.com>
2020-08-26 16:03:09 +0000

    help script added

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -4,3 +4,12 @@
 
 # Stop on error
 set -e
+
+HELP="
+$(basename "$0") [--project-name project_name] [--location location]
+
+commands:
+  -h [--help]                 show this help text
+  -p [--project-name]         name of the project, it'll be use to generate names for resources, leave blank to auto-generate a name
+  -l [--location]             location of the resources, westeurope, eastus, e.g.
+"
