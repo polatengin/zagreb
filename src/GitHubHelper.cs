b939a29c6e5931e60a176c218cd1ca8e69043c47
polatengin <polatengin@outlook.com>
2020-08-31 14:54:31 +0000

    initial helper class for github backend api

---
+++ src/GitHubHelper.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace zagreb
+{
+  public static class GitHubHelper
+  {
+  }
+}
