ffe9892e6201f236fb2ca2329d761d9ef1c80165
polatengin <polatengin@outlook.com>
2020-08-31 15:22:01 +0000

    using new github helpers to fetch prs and trigger workflows

--- src/PRCheckerScheduledFunction.cs
+++ src/PRCheckerScheduledFunction.cs
@@ -20,53 +20,28 @@ namespace zagreb
     {
       log.LogInformation("schedule...");
 
-      using (var client = new HttpClient())
+      var prs = await GitHubHelper.GetPullRequestsAsync();
+
+      foreach (var pr in prs)
       {
-        var GITHUB_PAT = Environment.GetEnvironmentVariable("GITHUB_PAT");
-        var GITHUB_ACCOUNT_NAME = Environment.GetEnvironmentVariable("GITHUB_ACCOUNT_NAME");
-        var GITHUB_REPO_NAME = Environment.GetEnvironmentVariable("GITHUB_REPO_NAME");
-        var GITHUB_ACTION_ID = Environment.GetEnvironmentVariable("GITHUB_ACTION_ID");
+        log.LogInformation($"{pr.Title} -> {string.Join(',', pr.Labels.Select(e => e.Name).ToList())}");
 
-        var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.github.com/repos/{GITHUB_ACCOUNT_NAME}/{GITHUB_REPO_NAME}/pulls")
+        var partitionKey = pr.Head.Repo.Name;
+        var rowKey = pr.Number.ToString();
+
+        pr.PartitionKey = partitionKey;
+        pr.RowKey = rowKey;
+
+        var result = table.Execute(TableOperation.Retrieve<PullRequest>(partitionKey, rowKey));
+
+        if (result?.Result == null)
         {
-          Headers = {
-            Authorization = new AuthenticationHeaderValue("token", GITHUB_PAT),
-            UserAgent = { ProductInfoHeaderValue.Parse("RunPipeline") },
-            Accept = { MediaTypeWithQualityHeaderValue.Parse("application/vnd.github.v3+json") }
-          }
-        };
-        using (var response = await client.SendAsync(request))
-        {
-          if (response.IsSuccessStatusCode)
-          {
-            var body = await response.Content.ReadAsStringAsync();
+          table.Execute(TableOperation.InsertOrMerge(pr));
 
-            var prs = JsonConvert.DeserializeObject<PullRequest[]>(body);
+          log.LogInformation("pr has been saved");
+        }
 
-            foreach (var pr in prs)
-            {
-              log.LogInformation($"{pr.Title} -> {string.Join(',', pr.Labels.Select(e => e.Name).ToList())}");
-
-              var partitionKey = pr.Head.Repo.Name;
-              var rowKey = pr.Number.ToString();
-
-              if (pr.Labels.Count(e => e.Name == "Azure") > 0)
-              {
-                var result = table.Execute(TableOperation.Retrieve<PullRequest>(partitionKey, rowKey));
-
-                if (result?.Result == null)
-                {
-                  pr.PartitionKey = partitionKey;
-                  pr.RowKey = rowKey;
-
-                  table.Execute(TableOperation.InsertOrMerge(pr));
-
-                  result.Result = pr;
-
-                  log.LogInformation("pr has been saved");
-                }
-
-                var fromTable = result?.Result as PullRequest;
+        var fromTable = result?.Result as PullRequest;
 
         if (fromTable == null || fromTable?.UpdatedAt != pr.UpdatedAt)
         {
