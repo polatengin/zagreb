75bba56a493dc561aa0101f644efbf2d22506eac
polatengin <polatengin@outlook.com>
2020-08-31 14:58:58 +0000

    helper function to trigger workflows

--- src/GitHubHelper.cs
+++ src/GitHubHelper.cs
@@ -42,5 +42,35 @@ namespace zagreb
         return Enumerable.Empty<PullRequest>();
       }
     }
+
+    public static async Task<bool> TriggerWorkflowAsync(string targetRepoName, string targetBranchName)
+    {
+      using (var client = new HttpClient())
+      {
+        var GITHUB_PAT = Environment.GetEnvironmentVariable("GITHUB_PAT");
+        var GITHUB_ACCOUNT_NAME = Environment.GetEnvironmentVariable("GITHUB_ACCOUNT_NAME");
+        var GITHUB_REPO_NAME = Environment.GetEnvironmentVariable("GITHUB_REPO_NAME");
+        var GITHUB_ACTION_ID = Environment.GetEnvironmentVariable("GITHUB_ACTION_ID");
+
+        var request = new HttpRequestMessage(HttpMethod.Post, $"https://api.github.com/repos/{GITHUB_ACCOUNT_NAME}/{GITHUB_REPO_NAME}/actions/workflows/{GITHUB_ACTION_ID}/dispatches")
+        {
+          Headers = {
+            Authorization = new AuthenticationHeaderValue("token", GITHUB_PAT),
+            UserAgent = { ProductInfoHeaderValue.Parse("RunPipeline") },
+            Accept = { MediaTypeWithQualityHeaderValue.Parse("application/vnd.github.v3+json") }
+          },
+          Content = new StringContent($"{{ \"ref\":\"master\", \"inputs\": {{\"repo\": \"{targetRepoName}\", \"branch\": \"{targetBranchName}\"}} }}")
+        };
+        using (var response = await client.SendAsync(request))
+        {
+          if (response.IsSuccessStatusCode)
+          {
+            return true;
+          }
+        }
+
+        return false;
+      }
+    }
   }
 }
