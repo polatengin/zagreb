0e1a950fd9f7f9e7a48bdd1c3d57ad10fa4b6c22
polatengin <polatengin@outlook.com>
2020-08-31 15:25:09 +0000

    github helper used to fetch the pr and trigger the workflow

--- src/PRListenerFunction.cs
+++ src/PRListenerFunction.cs
@@ -54,26 +54,11 @@ namespace zagreb
 
       log.LogInformation("CI pipeline is about to be triggered");
 
-      using (var client = new HttpClient())
-      {
-        var GITHUB_PAT = Environment.GetEnvironmentVariable("GITHUB_PAT");
-        var GITHUB_ACCOUNT_NAME = Environment.GetEnvironmentVariable("GITHUB_ACCOUNT_NAME");
-        var GITHUB_REPO_NAME = Environment.GetEnvironmentVariable("GITHUB_REPO_NAME");
-        var GITHUB_ACTION_ID = Environment.GetEnvironmentVariable("GITHUB_ACTION_ID");
+      var isTriggered = await GitHubHelper.TriggerWorkflowAsync(repo, branch);
 
-        var request = new HttpRequestMessage(HttpMethod.Post, $"https://api.github.com/repos/{GITHUB_ACCOUNT_NAME}/{GITHUB_REPO_NAME}/actions/workflows/{GITHUB_ACTION_ID}/dispatches")
-        {
-          Headers = {
-            Authorization = new AuthenticationHeaderValue("token", GITHUB_PAT),
-            UserAgent = { ProductInfoHeaderValue.Parse("RunPipeline") },
-            Accept = { MediaTypeWithQualityHeaderValue.Parse("application/vnd.github.v3+json") }
-          },
-          Content = new StringContent($"{{ \"ref\":\"master\", \"inputs\": {{\"repo\": \"{repo}\", \"branch\": \"{branch}\"}} }}")
-        };
-        using (var response = await client.SendAsync(request))
-        {
-          log.LogInformation($"CI pipeline is triggered, response is : {response.StatusCode} - {await response.Content.ReadAsStringAsync()}");
-        }
+      if (isTriggered)
+      {
+        log.LogInformation("CI pipeline is successfully triggered");
       }
 
       return new OkObjectResult("OK");
