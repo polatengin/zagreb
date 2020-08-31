4cb1d65f4acdd67e5817d3e40c5aaca4800b7efa
polatengin <polatengin@outlook.com>
2020-08-31 14:58:28 +0000

    helper function for fetching pull requests

--- src/GitHubHelper.cs
+++ src/GitHubHelper.cs
@@ -10,5 +10,37 @@ namespace zagreb
 {
   public static class GitHubHelper
   {
+    public static async Task<IEnumerable<PullRequest>> GetPullRequestsAsync()
+    {
+      using (var client = new HttpClient())
+      {
+        var GITHUB_PAT = Environment.GetEnvironmentVariable("GITHUB_PAT");
+        var GITHUB_ACCOUNT_NAME = Environment.GetEnvironmentVariable("GITHUB_ACCOUNT_NAME");
+        var GITHUB_REPO_NAME = Environment.GetEnvironmentVariable("GITHUB_REPO_NAME");
+        var GITHUB_ACTION_ID = Environment.GetEnvironmentVariable("GITHUB_ACTION_ID");
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.github.com/repos/{GITHUB_ACCOUNT_NAME}/{GITHUB_REPO_NAME}/pulls")
+        {
+          Headers = {
+            Authorization = new AuthenticationHeaderValue("token", GITHUB_PAT),
+            UserAgent = { ProductInfoHeaderValue.Parse("RunPipeline") },
+            Accept = { MediaTypeWithQualityHeaderValue.Parse("application/vnd.github.v3+json") }
+          }
+        };
+        using (var response = await client.SendAsync(request))
+        {
+          if (response.IsSuccessStatusCode)
+          {
+            var body = await response.Content.ReadAsStringAsync();
+
+            var prs = JsonConvert.DeserializeObject<PullRequest[]>(body);
+
+            return prs.Where(e => e.Labels.Count(e => e.Name == "Azure") > 0);
+          }
+        }
+
+        return Enumerable.Empty<PullRequest>();
+      }
+    }
   }
 }
