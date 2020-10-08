d855f747dcaa321a16b2da528b110c3c2bf4fb12
polatengin <enpolat@microsoft.com>
2020-10-08 13:23:22 +0000

    review payload added as an object

--- src/WebHookPayload.cs
+++ src/WebHookPayload.cs
@@ -166,6 +166,36 @@ namespace zagreb
     public long ChangedFiles { get; set; }
   }
 
+  public partial class Review
+  {
+    [JsonProperty("id")]
+    public long Id { get; set; }
+
+    [JsonProperty("node_id")]
+    public string NodeId { get; set; }
+
+    [JsonProperty("html_url")]
+    public Uri HtmlUrl { get; set; }
+
+    [JsonProperty("state")]
+    public string State { get; set; }
+
+    [JsonProperty("user")]
+    public Sender User { get; set; }
+
+    [JsonProperty("body")]
+    public string Body { get; set; }
+
+    [JsonProperty("submitted_at")]
+    public object SubmittedAt { get; set; }
+
+    [JsonProperty("_links")]
+    public Links Links { get; set; }
+
+    [JsonProperty("author_association")]
+    public string AuthorAssociation { get; set; }
+  }
+
   public partial class Label
   {
     [JsonProperty("id")]
